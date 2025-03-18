bfd4b53185e4d7703e5fd15889d872329f98294d
Syntax Surge <syntaxsurge@gmail.com>
2025-03-18 17:35:34 +0800

    update smart contract addresses

--- contracts/addresses.ts
+++ contracts/addresses.ts
@@ -17,11 +17,11 @@ type ContractAddresses = {
 
 export const CONTRACT_ADDRESSES: ContractAddresses = {
   [SUPPORTED_CHAINS.MOONBASE]: {
-    NFTMarketplaceHub: "0xc0397D24924ce99d2DE40e1224c7891c4f7aEB46",
-    PlatformRewardPool: "0x983a031A6f9C749cF4579c1c6D0D24b6b2aF992a",
-    UserExperiencePoints: "0x601499D51B3D136A0C1cF82522b88Da1B34a4426",
-    NFTCreatorCollection: "0x503438fDed04426beA9DD048A796a0973f1DaAD9",
-    NFTStakingPool: "0xf1d510fc12098E7b20C321e756864644E353c7DA",
+    NFTMarketplaceHub: "0xB031aCC58b182e34262FC7903d05e62AD30a48C8",
+    PlatformRewardPool: "0x07870EbBE687D98F5636b66c26e4005A854B8921",
+    UserExperiencePoints: "0x4264b772667f3e4d3dd5AB15D9c19E50E01AD55e",
+    NFTCreatorCollection: "0x8D875d4596f3BaA32552A5bf094f60446C864970",
+    NFTStakingPool: "0xde9f1Aa5E0C19fb224449C275E4A6E8B406e4c58",
     explorer: "https://moonbase.moonscan.io"
   }
 }
