46e3eaf38909ea0078dd8252d32be4848a2f26b2
Syntax Surge <syntaxsurge@gmail.com>
2025-03-23 08:09:19 +0800

    Explicitly ignore underscores (_) for unused variables in ESlint config

--- eslint.config.js
+++ eslint.config.js
@@ -77,7 +77,10 @@ export default [
                 },
             ],
             '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+            ],
             'prettier/prettier': 'warn',
         },
     },
